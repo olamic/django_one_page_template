cb8a4ac2b950fa1970299f23a40eeb6f986579b3
olamic <olawuyim338@gmail.com>
2022-12-15 12:17:39 -0800

    some new update

--- web/models.py
+++ web/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+    question_text= models.CharField(max_length=255)
+    pub_date = models.DateTimeField  ('date posted')
+
+class Author(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
